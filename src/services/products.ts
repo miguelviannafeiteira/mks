cf5691a387a218d0630a1f65ac0516ebfd998b64
Miguel Vianna <miguelviannafeiteira@gmail.com>
2023-11-16 13:12:46 -0300

    refactor: correção de nomenclatura

--- src/services/products.ts
+++ src/services/products.ts
@@ -1,7 +1,7 @@
 import { ProductsOutput } from "../types/productsModel";
 import { api } from "./api";
 
-export async function getProduts(): Promise<ProductsOutput> {
+export async function getProducts(): Promise<ProductsOutput> {
     const { data } = await api.get<ProductsOutput>("/products?page=1&rows=8&sortBy=id&orderBy=DESC")
     return data
 }
